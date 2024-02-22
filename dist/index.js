4d11f07040f1f5702603a47e7cd5cbb8637a2d89
Nathan Martin <63524975+nathan-i-martin@users.noreply.github.com>
2024-02-22 14:52:52 -0500

    implement more methods

---
+++ dist/index.js
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+
+const useGeneric = (initial) => {
+    const [state, setState] = useState(initial);
+    return {
+        get: () => state,
+        set: (value) => setState(value),
+    };
+};
+const useArray = (initial) => {
+    const state = useGeneric(initial ?? []);
+    return {
+        get: () => state.get(),
+        set: (array) => state.set(array),
+        getValue: (index) => state.get()[index],
+        getFirstValue: () => state.get()[0],
+        getLastValue: () => state.get()[state.get().length - 1],
+        push: (item) => state.set([...state.get(), item]),
+        pop: () => state.set(state.get().slice(0, -1)),
+        add: (item) => state.set([...state.get(), item]),
+        put: (index, value) => { const newArray = [...state.get()]; newArray[index] = value; state.set(newArray); },
+        remove: (index) => { const newArray = [...state.get()]; newArray.splice(index, 1); state.set(newArray); },
+        contains: (item) => state.get().includes(item),
+        size: () => state.get().length,
+        map: (callback) => state.get().map(callback),
+    };
+};
+const useSet = (initial) => {
+    const state = useGeneric(initial ?? new Set());
+    return {
+        get: () => state.get(),
+        set: (set) => state.set(set),
+        add: (item) => state.set(new Set([...state.get(), item])),
+        remove: (item) => { const newHashSet = new Set(state.get()); newHashSet.delete(item); state.set(newHashSet); },
+        contains: (item) => state.get().has(item),
+        size: () => state.get().size,
+        map: (callback) => Array.from(state.get()).map(callback),
+    };
+};
+const useMap = (initial) => {
+    const state = useGeneric(initial ?? new Map());
+    return {
+        get: () => state.get(),
+        set: (map) => state.set(map),
+        put: (key, value) => state.set(new Map(state.get()).set(key, value)),
+        remove: (key) => { const newMap = new Map(state.get()); newMap.delete(key); state.set(newMap); },
+        contains: (key) => state.get().has(key),
+        size: () => state.get().size,
+    };
+};
+const useNumber = (initial) => {
+    const state = useGeneric(initial ?? 0);
+    return {
+        get: () => state.get(),
+        set: (value) => state.set(value),
+        add: (value) => state.set(state.get() + value),
+        subtract: (value) => state.set(state.get() - value),
+        multiply: (value) => state.set(state.get() * value),
+        divide: (value) => state.set(state.get() / value),
+        mod: (value) => state.set(state.get() % value),
+        increment: () => state.set(state.get() + 1),
+        decrement: () => state.set(state.get() - 1),
+    };
+};
+const useBoolean = (initial) => {
+    const state = useGeneric(initial ?? false);
+    return {
+        get: () => state.get(),
+        set: (value) => state.set(value),
+        toggle: () => state.set(!state),
+    };
+};
+const useString = (initial) => {
+    const state = useGeneric(initial ?? "");
+    return {
+        get: () => state.get(),
+        set: (value) => state.set(value),
+        concat: (value) => state.set(state.get() + value),
+        append: (value) => state.set(state.get() + value),
+        prepend: (value) => state.set(value + state.get()),
+        size: () => state.get().length,
+    };
+};
+/*interface Optional<T> {
+    get: () => T,
+    getOrElse: (resolver?: ()=>T | T) => T,
+    getOrThrow: () => T,
+    exists: () => boolean
+}*/
+const State = {
+    useArray,
+    useSet,
+    useGeneric,
+    useMap,
+    useNumber,
+    useString,
+    useBoolean
+};
+
+export { State };
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,20 @@ const useArray = <V> (initial?: V[]) => {
     const state = useGeneric(initial ?? []);
 
     return {
-        push:       (item: V)       => state.set([...state.get(), item]),
-        pop:        ()              => state.set(state.get().slice(0, -1)),
-        add:        (item: V)       => state.set([...state.get(), item]),
-        remove:     (index: number) => state.set(state.get().filter((_, i) => i !== index)),
-        contains:   (item: V)       => state.get().includes(item),
-        size:       ()              => state.get().length,
+        get:            ()                          => state.get(),
+        set:            (array: V[])                => state.set(array),
+        getValue:       (index: number)             => state.get()[index],
+        getFirstValue:  ()                          => state.get()[0],
+        getLastValue:   ()                          => state.get()[state.get().length - 1],
+        push:           (item: V)                   => state.set([...state.get(), item]),
+        pop:            ()                          => state.set(state.get().slice(0, -1)),
+        add:            (item: V)                   => state.set([...state.get(), item]),
+        put:            (index: number, value: V)   => { const newArray = [...state.get()]; newArray[index] = value; state.set(newArray); },
+        remove:         (index: number)             => { const newArray = [...state.get()]; newArray.splice(index, 1); state.set(newArray); },
+        contains:       (item: V)                   => state.get().includes(item),
+        size:           ()                          => state.get().length,
 
-        map:        (callback: (item: V, index: number) => any) => state.get().map(callback),
+        map:            (callback: (item: V, index: number) => any) => state.get().map(callback),
     };
 }
 
@@ -28,6 +34,8 @@ const useSet = <V> (initial?: Set<V>) => {
     const state = useGeneric(initial ?? new Set<V>());
 
     return {
+        get:        ()                          => state.get(),
+        set:        (set: Set<V>)               => state.set(set),
         add:        (item: V)       => state.set(new Set([...state.get(), item])),
         remove:     (item: V)       => { const newHashSet = new Set(state.get()); newHashSet.delete(item); state.set(newHashSet); },
         contains:   (item: V)       => state.get().has(item),
@@ -41,6 +49,8 @@ const useMap = <K, V> (initial?: Map<K, V>) => {
     const state = useGeneric(initial ?? new Map<K, V>());
 
     return {
+        get:        ()                  => state.get(),
+        set:        (map: Map<K,V>)     => state.set(map),
         put:        (key: K, value: V)  => state.set(new Map(state.get()).set(key, value)),
         remove:     (key: K)            => { const newMap = new Map(state.get()); newMap.delete(key); state.set(newMap); },
         contains:   (key: K)            => state.get().has(key),
@@ -52,15 +62,25 @@ const useNumber = (initial?: number) => {
     const state = useGeneric(initial ?? 0);
 
     return {
-        get:        () =>              state.get(),
+        get:        ()              => state.get(),
         set:        (value: number) => state.set(value),
         add:        (value: number) => state.set(state.get() + value),
         subtract:   (value: number) => state.set(state.get() - value),
         multiply:   (value: number) => state.set(state.get() * value),
         divide:     (value: number) => state.set(state.get() / value),
         mod:        (value: number) => state.set(state.get() % value),
-        increment:  () =>              state.set(state.get() + 1),
-        decrement:  () =>              state.set(state.get() - 1),
+        increment:  ()              => state.set(state.get() + 1),
+        decrement:  ()              => state.set(state.get() - 1),
+    };
+}
+
+const useBoolean = (initial?: boolean) => {
+    const state = useGeneric(initial ?? false);
+
+    return {
+        get:    ()                  => state.get(),
+        set:    (value: boolean)    => state.set(value),
+        toggle: ()                  => state.set(!state),
     };
 }
 
@@ -73,8 +93,16 @@ const useString = (initial?: string) => {
         concat:     (value: string) => state.set(state.get() + value),
         append:     (value: string) => state.set(state.get() + value),
         prepend:    (value: string) => state.set(value + state.get()),
+        size:       ()              => state.get().length,
     };
 }
+
+/*interface Optional<T> {
+    get: () => T,
+    getOrElse: (resolver?: ()=>T | T) => T,
+    getOrThrow: () => T,
+    exists: () => boolean
+}*/
 
 const State = {
     useArray,
@@ -82,7 +110,8 @@ const State = {
     useGeneric,
     useMap,
     useNumber,
-    useString
+    useString,
+    useBoolean
 };
 
 export { State };
